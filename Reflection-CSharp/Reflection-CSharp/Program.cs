94bcc3706b20a227b855e625419ceb9d5d41f44d
aaronamm <theeranitp@gmail.com>
2018-01-29 07:18:26 +0700

    initialize reflection demo in C#

---
+++ Reflection-CSharp/Reflection-CSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_CSharp
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            user.FirstName = "Aaron";
+            user.LastName = "Pongtongmuang";
+          Console.WriteLine(string.Format("full name: {0}", user.GetFullName()));
+        }
+    }
+}
